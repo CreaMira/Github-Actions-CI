54fc69ebc10019d21bb9965b795f66a77896849c
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-29 13:46:30 -0700

    flake8 fixing
    ./test.py:11:5: E303 too many blank lines (2)
    1     E303 too many blank lines (2)
    1
    Error: Process completed with exit code 1.

--- test.py
+++ test.py
@@ -6,8 +6,7 @@ class TestCase(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(example.add(1, 2), 3)
-
-
+    
     def test_mul(self):
         self.assertEqual(example.mul(3, 4), 12)
 
